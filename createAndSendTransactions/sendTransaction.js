389688a29433641762a9390e86220e9dc1473807
Owen <owenventer99@gmail.com>
2023-05-22 11:40:53 +0200

    updated sendTransaction

--- createAndSendTransactions/sendTransaction.js
+++ createAndSendTransactions/sendTransaction.js
@@ -5,7 +5,7 @@ const connection = new solanaWeb3.Connection(
     'confirmed',
   );
 
-const sender=solanaWeb3.Keypair.fromSecretKey(new Uint8Array([61,155,131,194,164,104,243,153,197,74,84,107,225,184,5,40,64,186,91,10,132,193,118,98,124,40,90,79,117,200,183,226,10,121,246,225,15,12,144,58,143,127,191,39,191,101,85,219,28,31,4,228,83,122,178,22,212,16,220,211,137,100,60,49]));
+const sender=solanaWeb3.Keypair.fromSecretKey(new Uint8Array([61,...60,49]));
 
 // Set the address that will receive the SOL
 const to = new solanaWeb3.PublicKey("86xCnPeV69n6t3DnyGvkKobf9FdN2H9oiVDdaMpo2MMY");
